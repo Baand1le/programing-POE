038efe8f8a51ec6a080d36daacfe929e87de881f
Baand1le <ST10487923@rcconnect.edu.za>
2025-04-10 08:26:37 +0000

    Initial commit

---
+++ src/programing/poe/ProgramingPOE.java
@@ -0,0 +1,13 @@
+package programing.poe;
+
+import java.util.Scanner;
+
+public class ProgramingPOE {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please create a username");
+        
+    }
+    
+}
